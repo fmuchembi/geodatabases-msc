357e3a93f4f836da0223149e9ca93120093001f8
unknown <fmuchembi@gmail.com>
2025-02-09 11:49:05 +0100

    delete commented out text

--- housing/views.py
+++ housing/views.py
@@ -70,7 +70,6 @@ class GetDistrictsWithHostels(View):
             if has_address:
                 districts_with_addresses.add(district.name)
         
-        # Format results
         results = [{'name': name} for name in districts_with_addresses]
         
         print(f"Found {len(results)} districts containing addresses")
@@ -80,6 +79,13 @@ class GetDistrictsWithHostels(View):
             'count': len(results),
             'districts': results
         }, safe=False)
+ 
+
+
+
+
+
+
 
 
 
@@ -93,7 +99,6 @@ class GetDistrictsWithHostels(View):
 
 
 
-    
 
 
 
@@ -124,75 +129,3 @@ class GetDistrictsWithHostels(View):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''class FilterHousesView(View):
-    def get(self, request):
-        near_bus_stop = request.GET.get('near_bus_stop', '').lower() == 'true'
-        near_outdoor_activities = request.GET.get('near_outdoor_activities', '').lower() == 'true'
-        #outdoor_soccer = request.GET.get('outdoor_soccer', '').lower() == 'true'
-        #outdoor_basketball = request.GET.get('outdoor_basketball', '').lower() == 'true'
-        #outdoor_volleyball = request.GET.get('outdoor_volleyball', '').lower() == 'true'
-        #outdoor_athletics = request.GET.get('outdoor_athletics', '').lower() == 'true'
-        #near_university = request.GET.get('near_university', '').lower() == 'true'
-        #near_green_spaces = request.GET.get('near_green_spaces', '').lower() == 'true'
-       
-
-        houses = filter_houses(
-            near_bus_stop=near_bus_stop,
-            near_outdoor_activities=near_outdoor_activities,
-            #near_university=near_university,
-            #near_green_spaces=near_green_spaces,
-           
-        )
-
-    houses = filter_houses(
-            near_bus_stop=near_bus_stop,
-            outdoor_soccer=outdoor_soccer,
-            outdoor_basketball=outdoor_basketball,
-            outdoor_volleyball=outdoor_volleyball,
-            outdoor_athletics=outdoor_athletics,
-            #district=district,
-            #niversity_building=university_building,
-            #near_green_spaces=near_green_spaces,
-           # min_price=min_price,
-            #max_price=max_price,
-        )
-
-        # Serialize the results
-        results = [
-            {
-                'id': house.id,
-                'name': house.name,
-                'latitude': house.geom.y,
-                'longitude': house.geom.x,
-            }
-            for house in houses
-        ]
-
-        return JsonResponse(results, safe=False)'''
